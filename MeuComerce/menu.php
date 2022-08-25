d4e733d47d5d571c348a0b9b288cfc2750db5f15
GabrielLucasGirardi <ggirardi111@gmail.com>
2022-08-25 14:36:58 -0300

    pasta de MeuComerce

---
+++ MeuComerce/menu.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-expand-lg bg-light">
+    <a href="?pagina=listar" class="btn btn-info">Lista</a>
+    <a href="?pagina=cadastros" class="btn btn-success">Cadastros</a>
+</nav>
+<hr>
